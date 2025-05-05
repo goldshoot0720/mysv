d802d5425639d7ae7e456d9f829bcfa41408500b
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-06 01:29:20 +0800

    mysv init 003 Zap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import BuySoftwareHistory from "./buySoftwareHistory.svelte";
 	import AnimeOPED from "./animeOPED.svelte";
 	import WeartherWeek from "./weartherWeek.svelte";
+	import { Zap } from 'lucide-svelte';
 	let showBuy10000 = false;
 	let showBuy370000 = false;
 	let showBuySoftwareHistory = false;
@@ -16,16 +17,11 @@
 <br/>
 The future I see，{Math.abs(Math.floor((new Date()-new Date("2025-07-05"))/1000/3600/24))}天，考試因故延期至十月，榜示因故延期至明年一月十四日
 <br/>
-<button class="hover:text-blue-500" onclick={() => { showBuy10000 = !showBuy10000; }}>Buy10000</button>
-<br/>
-<button class="hover:text-blue-500" onclick={() => { showBuy370000 = !showBuy370000; }}>Buy370000</button>
-<br/>
-<button class="hover:text-blue-500" onclick={() => { showBuySoftwareHistory = !showBuySoftwareHistory; }}>BuySoftwareHistory</button>
-<br/>
-<button class="hover:text-blue-500" onclick={() => { showAnimeOPED = !showAnimeOPED; }}>2021~2025 TVアニメ OP ED</button>
-<br/>
-<button class="hover:text-blue-500" onclick={() => { showWeartherWeek = !showWeartherWeek; }}>中壢區一週天氣預報</button>
-<br/>
+<button class="flex items-center gap-1 hover:text-blue-500" onclick={() => { showBuy10000 = !showBuy10000; }}><Zap />Buy10000</button>
+<button class="flex items-center gap-1 hover:text-blue-500" onclick={() => { showBuy370000 = !showBuy370000; }}><Zap />Buy370000</button>
+<button class="flex items-center gap-1 hover:text-blue-500" onclick={() => { showBuySoftwareHistory = !showBuySoftwareHistory; }}><Zap />BuySoftwareHistory</button>
+<button class="flex items-center gap-1 hover:text-blue-500" onclick={() => { showAnimeOPED = !showAnimeOPED; }}><Zap />2021~2025 TVアニメ OP ED</button>
+<button class="flex items-center gap-1 hover:text-blue-500" onclick={() => { showWeartherWeek = !showWeartherWeek; }}><Zap />中壢區一週天氣預報</button>
 {#if showBuy10000}
 <Buy10000 />
 {/if}
