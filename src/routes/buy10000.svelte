b528955ddfba4e25983a5614616e778fb43df014
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-12 15:31:07 +0800

    mysv books

--- src/routes/buy10000.svelte
+++ src/routes/buy10000.svelte
@@ -3,37 +3,37 @@
 		{
 			name: '圖解考試的科學：高效率學習的關鍵',
 			src: 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/096/47/0010964719.jpg&v=64ca306ck&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010964719?sloc=main',
+			link: 'https://www.books.com.tw/products/0010964719?sloc=main',
 			price: 432
 		},
 		{
 			name: '法律人的第一本書（7版）',
 			src: 'https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/099/24/0010992430.jpg&v=666c1bb8k&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010992430?sloc=main',
+			link: 'https://www.books.com.tw/products/0010992430?sloc=main',
 			price: 380
 		},
 		{
 			name: '圖解憲法：國家考試的第一本書（第五版）',
 			src: 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/099/91/0010999157.jpg&v=66c7135dk&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010999157?sloc=main',
+			link: 'https://www.books.com.tw/products/0010999157?sloc=main',
 			price: 675
 		},
 		{
 			name: '圖解民法：國家考試的第一本書（第五版）',
 			src: 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/093/99/0010939969.jpg&v=6357bafak&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010939969?sloc=main',
+			link: 'https://www.books.com.tw/products/0010939969?sloc=main',
 			price: 675
 		},
 		{
 			name: '圖解刑法：國家考試的第一本書（第七版）',
 			src: 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/098/33/0010983319.jpg&v=65d7224ck&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010983319?sloc=main',
+			link: 'https://www.books.com.tw/products/0010983319?sloc=main',
 			price: 612
 		},
 		{
 			name: '圖解行政法：國家考試的第一本書（第五版）',
 			src: 'https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/097/49/0010974918.jpg&v=655f2a30k&w=348&h=348',
-			link: 'https://www.buys.com.tw/products/0010974918?sloc=main',
+			link: 'https://www.books.com.tw/products/0010974918?sloc=main',
 			price: 675
 		},
 		{
